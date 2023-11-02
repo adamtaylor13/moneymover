cd0eec1888586b61511c51db7d72b8ad55af1d3b
Adam Taylor <adamtaylor0795@gmail.com>
2023-11-02 11:01:03 -0500

    Fix tests broken from recent change to getNextMonthUrl

--- src/utils/monthNavigation.test.ts
+++ src/utils/monthNavigation.test.ts
@@ -3,17 +3,17 @@ import { getNextMonthUrl, getPreviousMonthUrl } from "./monthNavigation";
 describe("monthNavigation", () => {
   describe("getNextMonthUrl", () => {
     it("should return the correct URL for the next month", () => {
-      expect(getNextMonthUrl(1, 2022)).toEqual("/transactions/2022/02");
-      expect(getNextMonthUrl(12, 2022)).toEqual("/transactions/2023/01");
-      expect(getNextMonthUrl(9, 2022)).toEqual("/transactions/2022/10");
+      expect(getNextMonthUrl(1, 2022)).toEqual("/2022/02");
+      expect(getNextMonthUrl(12, 2022)).toEqual("/2023/01");
+      expect(getNextMonthUrl(9, 2022)).toEqual("/2022/10");
     });
   });
 
   describe("getPreviousMonthUrl", () => {
     it("should return the correct URL for the previous month", () => {
-      expect(getPreviousMonthUrl(2, 2022)).toEqual("/transactions/2022/01");
-      expect(getPreviousMonthUrl(11, 2022)).toEqual("/transactions/2022/10");
-      expect(getPreviousMonthUrl(1, 2022)).toEqual("/transactions/2021/12");
+      expect(getPreviousMonthUrl(2, 2022)).toEqual("/2022/01");
+      expect(getPreviousMonthUrl(11, 2022)).toEqual("/2022/10");
+      expect(getPreviousMonthUrl(1, 2022)).toEqual("/2021/12");
     });
   });
 });
