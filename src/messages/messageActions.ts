39250a0f0904ba1bd556560b0a394f7b1f5890a8
Adam Taylor <adamtaylor0795@gmail.com>
2023-11-03 15:56:39 -0500

    Migrate more usages for bullet-proofness

--- src/messages/messageActions.ts
+++ src/messages/messageActions.ts
@@ -1,21 +1,25 @@
-import { getElementsForKey } from "@utils/getElementForKey";
+import { getElementsForUnknownKey } from "@utils/getElementForKey";
 
 export function highlightElement(key: string) {
-  const elements = getElementsForKey(key);
-  for (const element of elements) {
-    element?.style.setProperty(
-      "box-shadow",
-      "0px 0px 4px 4px #fbb700",
-      "important"
-    );
-    element?.style.setProperty("border-radius", "10px");
+  const elements = getElementsForUnknownKey(key);
+  if (Array.isArray(elements)) {
+    for (const element of elements) {
+      element?.style.setProperty(
+        "box-shadow",
+        "0px 0px 4px 4px #fbb700",
+        "important"
+      );
+      element?.style.setProperty("border-radius", "10px");
+    }
   }
 }
 
 export function unhighlightElement(key: string) {
-  const elements = getElementsForKey(key);
-  for (const element of elements) {
-    element?.style.removeProperty("box-shadow");
-    element?.style.removeProperty("border-radius");
+  const elements = getElementsForUnknownKey(key);
+  if (Array.isArray(elements)) {
+    for (const element of elements) {
+      element?.style.removeProperty("box-shadow");
+      element?.style.removeProperty("border-radius");
+    }
   }
 }
