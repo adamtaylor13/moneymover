65cdb37f077b8f50b969bda9bb1a5acd5b948573
Adam Taylor <adamtaylor0795@gmail.com>
2023-11-04 09:50:01 -0500

    Add more refactoring in preparation for testing

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -4,85 +4,86 @@ import { Sequencer } from "@utils/Sequencer";
 
 initializeMessageListener();
 
-const seq = Sequencer();
-
-const keyDownEventListener = function (event: KeyboardEvent) {
-  // In general, we probably don't want to run any of these functions
-  // when "ctrl" or "meta" (cmd on MacOS) is held down. Reason being is that
-  // these actually do things in the browser. So for example, Ctrl/Meta-R will
-  // refresh the page, and chances are that's _ALL_ the user intends for it to do.
-  if (
+const keyDownEventListener = (seq: ReturnType<typeof Sequencer>) =>
+  function (event: KeyboardEvent) {
+    // In general, we probably don't want to run any of these functions
+    // when "ctrl" or "meta" (cmd on MacOS) is held down. Reason being is that
+    // these actually do things in the browser. So for example, Ctrl/Meta-R will
+    // refresh the page, and chances are that's _ALL_ the user intends for it to do.
+    if (
       document.activeElement instanceof HTMLInputElement ||
       event.ctrlKey ||
       event.metaKey
-  ) {
-    return;
-  }
+    ) {
+      return;
+    }
 
-  /**
-   * So far, all of our keys are one-shot keybindings. That means as soon as
-   * we press them, they activate. However, we'd like to get the full vim-like,
-   * modal editing experience. To do this, we need to figure out how to keep
-   * our one-shot keybindings, but also allow for sequential keybindings.
-   */
-  seq.add(event.key);
-  if (seq.isBuildingValidSequence()) {
-    if (seq.hasCompletedSequence()) {
-      seq.handleSequence();
-    }
-    return;
-  }
-
-  // else it's a one-shot keybinding
-  switch (event.key) {
-    case "n": {
-      const nextMonthButton = getElementsForKey("n");
-      nextMonthButton?.click();
-      break;
-    }
-    case "p": {
-      const previousMonthButton = getElementsForKey("p");
-      previousMonthButton?.click();
-      break;
-    }
-    case "r": {
-      const reviewTransactionsButton = getElementsForKey("r");
-      reviewTransactionsButton?.click();
-      break;
-    }
-    case "t": {
-      const backToThisMonthButton = getElementsForKey("t");
-      backToThisMonthButton?.click();
-      break;
-    }
-    case "/": {
-      event.preventDefault(); // Don't actually type this key
-      const quickFilterInput = getElementsForKey("/");
-      quickFilterInput?.focus();
-      quickFilterInput?.select();
-      break;
-    }
-    case "Escape": {
-      seq.reset(); // Escape will reset our sequence
-      const [clearFilterButton, closeModalButton] = getElementsForKey("Escape");
-      clearFilterButton?.click();
-      closeModalButton?.click();
-      break;
-    }
-    case "Tab": {
-      const isShifted = event.shiftKey;
-      event.preventDefault();
-      const [previousNavElement, nextNavElement] = getElementsForKey("Tab");
-
-      if (isShifted) {
-        previousNavElement?.click();
-      } else {
-        nextNavElement?.click();
+    /**
+     * So far, all of our keys are one-shot keybindings. That means as soon as
+     * we press them, they activate. However, we'd like to get the full vim-like,
+     * modal editing experience. To do this, we need to figure out how to keep
+     * our one-shot keybindings, but also allow for sequential keybindings.
+     */
+    seq.add(event.key);
+    if (seq.isBuildingValidSequence()) {
+      if (seq.hasCompletedSequence()) {
+        seq.handleSequence();
       }
-      break;
+      return;
     }
-  }
-};
 
-document.addEventListener("keydown", keyDownEventListener);
+    // else it's a one-shot keybinding
+    switch (event.key) {
+      case "n": {
+        const nextMonthButton = getElementsForKey("n");
+        nextMonthButton?.click();
+        break;
+      }
+      case "p": {
+        const previousMonthButton = getElementsForKey("p");
+        previousMonthButton?.click();
+        break;
+      }
+      case "r": {
+        const reviewTransactionsButton = getElementsForKey("r");
+        reviewTransactionsButton?.click();
+        break;
+      }
+      case "t": {
+        const backToThisMonthButton = getElementsForKey("t");
+        backToThisMonthButton?.click();
+        break;
+      }
+      case "/": {
+        event.preventDefault(); // Don't actually type this key
+        const quickFilterInput = getElementsForKey("/");
+        quickFilterInput?.focus();
+        quickFilterInput?.select();
+        break;
+      }
+      case "Escape": {
+        seq.reset(); // Escape will reset our sequence
+        const [clearFilterButton, closeModalButton] =
+          getElementsForKey("Escape");
+        clearFilterButton?.click();
+        closeModalButton?.click();
+        break;
+      }
+      case "Tab": {
+        const isShifted = event.shiftKey;
+        event.preventDefault();
+        const [previousNavElement, nextNavElement] = getElementsForKey("Tab");
+
+        if (isShifted) {
+          previousNavElement?.click();
+        } else {
+          nextNavElement?.click();
+        }
+        break;
+      }
+    }
+  };
+
+const sequencer = Sequencer();
+document.addEventListener("keydown", keyDownEventListener(sequencer));
 console.log("🪄 MoneyMover intialized.");
